5a40916c5718189f6141cdbfa6293e6967022a38
ansa52 <100661892+ansa52@users.noreply.github.com>
2022-03-30 15:25:18 +0700

    Update publish.r

--- publish.r
+++ publish.r
@@ -12,22 +12,28 @@ con <- dbConnect(drv,
 )
 
 
-query <- 'SELECT * FROM "public"."posisi"'
+query <- 'SELECT * FROM "public"."activity" ORDER BY "startDateLocal" DESC LIMIT 1'
 data <- dbGetQuery(con, query)
+
+query1 <- paste0('SELECT * FROM "public"."posisi_new" WHERE "id" = ',data$id)
+posisi <- dbGetQuery(con, query1)
 
 ## animate plot
 library(gganimate)
 library(ggplot2)
 
-p <- ggplot(data, aes(x=lng,y=lat)) + geom_path() + geom_point(aes(group = time)) +
+p <- ggplot(posisi, aes(x=V2,y=V1)) + geom_path() + geom_point(aes(group = time)) +
   transition_reveal(along = time)
 p <- animate(p,renderer = gifski_renderer())
 anim_save("anime.gif", animation = p)
 
+
 ## Status Message
 status_details <- paste0(
-  Sys.Date(), "\n",
-  "Strava Plot", "\n"
+  "Name: ", data$name, "\n",
+  "Distance: ", data$distance, " km\n",
+  "Elevation: ", data$elevation, " m\n",
+  "Moving Time: ", data$movingTime, "\n"
 )
 
 # Publish to Twitter
